fac607bb4197ee70e6d8051ec8977c05eb13500c
J.Mrudula <J.Mrudula@192.168.13.18>
2020-03-02 14:30:11 +0530

    created a new project

---
+++ git/src/main/java/com/epam1/git/App.java
@@ -0,0 +1,13 @@
+package com.epam1.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
